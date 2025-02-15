ce305512643e5860c85918ca6a19111669da4a56
Md. Baktiyar -Ul- Alam <baktiyar.sagar@gmail.com>
2025-02-15 19:51:48 +0600

    Implement Complete button and Exit button

--- gui.py
+++ gui.py
@@ -7,11 +7,17 @@ add_button = fg.Button("Add")
 list_box = fg.Listbox(values=functions.get_todos(),key='todo_box',
                       enable_events=True, size=[44,10])
 edit_button = fg.Button("Edit")
+complete_button = fg.Button("Complete")
+exit_button = fg.Button("Exit")
+
 window = fg.Window("To do app",
-                   layout=[[label],[text_box,add_button],[list_box,edit_button]],
+                   layout=[[label],[text_box,add_button],
+                           [list_box,edit_button,complete_button],
+                           [exit_button]],
                    font=('Helvetica',12))
 while True:
     event , value = window.read()
+    print(event,value)
     print (event)
     print(value)
     # print(value['todo_box'])
@@ -22,6 +28,7 @@ while True:
             todo_list.append(new_todo)
             functions.write_todo(todo_list)
             window['todo_box'].update(values=todo_list)
+            window['todo'].update(value='')
         case 'Edit':
             todo_to_edit = value['todo_box'][0]
             new_todo = value['todo']+'\n'
@@ -30,6 +37,16 @@ while True:
             todo_list[index] = new_todo
             functions.write_todo(todo_list)
             window['todo_box'].update(values=todo_list)
+            window['todo'].update(value='')
+        case 'Complete':
+            todo_to_complete = value['todo_box'][0]
+            todo_list = functions.get_todos()
+            todo_list.remove(todo_to_complete)
+            functions.write_todo(todo_list)
+            window['todo_box'].update(values=todo_list)
+            window['todo'].update(value='')
+        case 'Exit':
+            break
 
         case 'todo_box':
             window['todo'].update(value=value['todo_box'][0].strip('\n'))
